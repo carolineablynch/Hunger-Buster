56853f4d3699360a62b7d4cbf519061911ed6083
Donnahue George <Donnahuegjr@gmail.com>
2020-12-17 22:09:05 -0500

    updated page with new zomato API

--- javascript.js
+++ javascript.js
@@ -20,7 +20,16 @@ $("#randomFood").on("click", function() {
         $("#foodphoto1").empty();
         $("#foodphoto1").append(foodImg);
 
-
+        if (imageUrl.toString().includes("pizza", 0)) {
+            var cuisineType = "Italian"
+            console.log(cuisineType)
+        } else if (imageUrl.toString().includes("burger", 0)) {
+            var cuisineType = "American"
+            console.log(cuisineType)
+        } else {
+            var cuisineType = "Indian"
+            console.log(cuisineType)
+        }
     });
 });
 
